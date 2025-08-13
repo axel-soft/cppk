a062489887b61303253e7643dcf44ed47c76c41e
Елбашов Дмитрий Иванович <delbashov@axelsoft.ru>
2025-08-13 12:41:58 +0300

    13.08.2025

--- tools/build.cmd
+++ tools/build.cmd
@@ -16,29 +16,30 @@ set EXT_NAME="ЦППК"
 REM Путь к каталогу с XML-файлами расширения
 set XML_FOLDER_E="G:\Git\cppk\src\cfe"
 
-set hran="Z:\hran_1c\hran_sed30\conf"
-set hran_e="Z:\hran_1c\hran_sed30\ext" 
+set hran="C:\hran_1c\hran_sed30\conf"
+set hran_e="C:\hran_1c\hran_sed30\ext" 
 
 cd /d %REPO%
 
 echo Получение из хранилища ...
-%CONFIG_EXE% CONFIG %BASE% /ConfigurationRepositoryUpdateCfg 
+rem %CONFIG_EXE% CONFIG %BASE% /ConfigurationRepositoryUpdateCfg 
 REM /ConfigurationRepositoryF "Z:\hran_1c\hran_sed30\conf" /ConfigurationRepositoryN cppk_do30_dev_03
 
 echo Получение из хранилища расширения ...
-%CONFIG_EXE% CONFIG %BASE% /ConfigurationRepositoryUpdateCfg -Extension %EXT_NAME% 
+rem %CONFIG_EXE% CONFIG %BASE% /ConfigurationRepositoryUpdateCfg -Extension %EXT_NAME% 
 rem /ConfigurationRepositoryF "Z:\hran_1c\hran_sed30\ext" /ConfigurationRepositoryN cppk_do30_dev_03
 
 echo Конфигурация успешно загружена, обновляем БД...
-%CONFIG_EXE% CONFIG %BASE% /UpdateDBCfg
+rem %CONFIG_EXE% CONFIG %BASE% /UpdateDBCfg
 
 echo Выгрузка конфигурации в XML
-%CONFIG_EXE% CONFIG %BASE% /DumpConfigToFiles %XML_FOLDER%
+rem %CONFIG_EXE% CONFIG %BASE% /DumpConfigToFiles %XML_FOLDER%
 
 echo Выгрузка расширения в XML
-%CONFIG_EXE% CONFIG %BASE% /DumpConfigToFiles %XML_FOLDER_E% -Extension %EXT_NAME%
+rem %CONFIG_EXE% CONFIG %BASE% /DumpConfigToFiles %XML_FOLDER_E% -Extension %EXT_NAME%
 
-set commit ="auto commit " %DATE% %TIME%
+set commit="auto commit " %DATE% %TIME%
+echo %commit%
 git add .
-git commit -m %commit%
+git commit -m ""%commit%""
 git push
