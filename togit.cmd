9a1091924431fce3b01a357e98edc03bdba597d1
Елбашов Дмитрий Иванович <delbashov@axelsoft.ru>
2025-08-25 16:43:39 +0300

    to git

--- togit.cmd
+++ togit.cmd
@@ -1,9 +1,9 @@
-REM РџРµСЂРµС…РѕРґРёРј РІ РїР°РїРєСѓ РїСЂРѕРµРєС‚Р°
+REM Переходим в папку проекта
 cd /d C:\Git\cppk
 
-REM РџРѕР»СѓС‡Р°РµРј РёР·РјРµРЅРµРЅРёСЏ РёР· С…СЂР°РЅРёР»РёС‰
-call gitsync sync -u gitbot -e "Р¦РџРџРљ" C:\hran_1c\hran_sed30\ext C:\Git\cppk\src\cfe
+REM Получаем изменения из хранилищ
+call gitsync sync -u gitbot -e "ЦППК" C:\hran_1c\hran_sed30\ext C:\Git\cppk\src\cfe
 call gitsync sync -u gitbot C:\hran_1c\hran_sed30\conf C:\Git\cppk\src\cf
 
-REM РћС‚РїСЂР°РІР»СЏРµРј РёР·РјРµРЅРµРЅРёСЏ РІ Git
+REM Отправляем изменения в Git
 git push
